609ea9bdd6b12f918c63882ff42cae09c580b10d
Eby <eby.sofyan@quokkahr.com>
2023-04-08 23:04:28 +0800

    add user repository abstraction

---
+++ src/core/ports/repositories/user_repository.py
@@ -0,0 +1,16 @@
+from typing import Protocol, Any
+from ...domain.user import User
+
+
+class UserRepository(Protocol):
+    def get_by_id(self, id: int) -> User | None:
+        ...
+
+    def get_by_username(self, username: str) -> User | None:
+        ...
+
+    def get_all(self) -> list[User]:
+        ...
+
+    def create(self, payload: dict[str, Any]) -> User:
+        ...
